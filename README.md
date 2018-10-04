c4983bad4e47a4d4f2bd491740c4b9c286473581
heowc <heowc1992@gmail.com>
2018-10-04 18:58:39 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Summary
+- tdd example using spock
+
+# Spec
+- java 1.8
+- groovy 2.4
+- spring boot 2.0.5
+- spring data jdbc 1.0.0
+- spock 1.2
+- h2
+
+# Link
+- http://spockframework.org/spock/docs/1.2/all_in_one.html
+- https://github.com/spockframework/spock
---
+++ src/test/groovy/com/heowc/GroovyApplicationTests.groovy
@@ -0,0 +1,18 @@
+package com.heowc
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import spock.lang.Specification
+
+@SpringBootTest(classes = Application)
+class GroovyApplicationTests extends Specification {
+
+    @Autowired
+    ApplicationContext context
+
+    def "contextLoads"() {
+        expect:
+        context != null
+    }
+}
