e318c0b043a49b2d568d7999468f4c2fc5537922
heowc <heowc1992@gmail.com>
2018-10-18 20:16:46 +0900

    [ADD] EditPostService 추가

---
+++ src/test/java/com/heowc/post/EditPostServiceTest.java
@@ -0,0 +1,74 @@
+package com.heowc.post;
+
+import com.heowc.config.TestConfig;
+import com.heowc.config.TestConstant;
+import org.codehaus.groovy.runtime.DefaultGroovyMethods;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Import(TestConfig.class)
+public class EditPostServiceTest {
+
+    private EditPostService service;
+
+    @Autowired
+    private PostRepository repository;
+
+    @Before
+    public void before_setup() {
+        service = new SimpleEditPostService(repository);
+    }
+
+    @Test
+    public void test_동일한_글쓴이ID가_아니므로_실패() {
+        PostRequest postRequest = new PostRequest("제목", "본문");
+        Post post = repository.save(postRequest.toPost());
+
+        assertThatThrownBy(
+            () -> service.edit(new Post(post.getId(), "수정된 제목", "수정된 본문", TestConstant.ID + 1, null, null))
+        ).isInstanceOf(AccessDeniedException.class);
+    }
+
+    @Test
+    public void test_없는_게시물을_지우려고_하므로_실패() {
+
+        assertThatThrownBy(
+            () -> service.edit(new Post(1L, "수정된 제목", "수정된 본문", TestConstant.ID, null, null))
+        ).isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    public void test_동일한_글쓴이ID가_수정하여_성공() {
+        PostRequest postRequest = new PostRequest("제목", "본문");
+        Post post = repository.save(postRequest.toPost());
+
+        Post updatedPost = service.edit(new Post(post.getId(), "수정된 제목", "수정된 본문", TestConstant.ID, null, null));
+
+        Optional<Post> byId = repository.findById(updatedPost.getId());
+
+        assertThat(byId).hasValueSatisfying(p -> {
+            assertThat(p).hasFieldOrPropertyWithValue("title", updatedPost.getTitle());
+            assertThat(p).hasFieldOrPropertyWithValue("content", updatedPost.getContent());
+            assertThat(p).hasFieldOrPropertyWithValue("createdBy", updatedPost.getCreatedBy());
+        });
+    }
+
+    @After
+    public void after_cleanup() {
+        repository.deleteAll();
+    }
+}
