86e4693eb19bec4f20dda8378fa0c6f662539cee
heowc <heowc1992@gmail.com>
2018-10-22 17:38:51 +0900

    [ADD] Post 삭제에 대한 테스트 케이스 작성

--- src/main/java/com/heowc/post/SimpleRemovePostService.java
+++ src/main/java/com/heowc/post/SimpleRemovePostService.java
@@ -5,6 +5,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class SimpleRemovePostService implements RemovePostService {
 
+    private final PostRepository repository;
+
+    public SimpleRemovePostService(PostRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public void remove(Post post) {
 
--- src/test/groovy/com/heowc/post/RemovePostServiceSpec.groovy
+++ src/test/groovy/com/heowc/post/RemovePostServiceSpec.groovy
@@ -13,19 +13,49 @@ class RemovePostServiceSpec extends Specification {
     PostRepository repository
 
     def setup() {
-        service = new SimpleEditPostService(repository)
+        service = new SimpleRemovePostService(repository)
     }
     
     def "동일한 글쓴이 ID가 아니므로 실패"() {
+        given:
+        def post = new Post(1L, "제목", "본문", "heowc", null, null)
+        repository.save(post)
 
+        when:
+        service.remove(new Post(1L, "제목", "본문", "wonchul", null, null))
+
+        then:
+        thrown(AccessDeniedException.class)
+        def byId = repository.findById(post.id)
+        !byId.empty()
+        post.id == byId.get().id
+        post.title == byId.get().title
+        post.content == byId.get().content
+        post.createdBy == byId.get().createdBy
     }
 
     def "없는 게시물을 지우려고 하므로 실패"() {
+        given:
 
+        when:
+        service.remove(new Post(1L, "제목", "본문", "heowc", null, null))
+
+        then:
+        thrown(NoSuchElementException.class)
     }
     
     def "동일한 글쓴이 ID가 삭제하여 성공"() {
-        
+        given:
+        def post = new Post(1L, "제목", "본문", "heowc", null, null)
+        repository.save(post)
+
+        when:
+        service.remove(new Post(1L, null, null, "heowc", null, null))
+
+        then:
+        notThrown(AccessDeniedException.class)
+        def byId = repository.findById(post.id)
+        byId.empty()
     }
 
     def cleanup() {
