723d9b7178ee8f6e3894052af62ddfb29f2075c7
heowc <heowc1992@gmail.com>
2018-11-05 10:23:41 +0900

    [ADD] Post 필드 변경에 대한 테스트 케이스 추가

---
+++ src/test/groovy/com/heowc/post/PostSpec.groovy
@@ -0,0 +1,23 @@
+package com.heowc.post
+
+import spock.lang.Specification
+
+class PostSpec extends Specification {
+
+    def "Post의 title과 content 수정 성공"() {
+        given:
+        def post = new Post(1L, "제목", "본문", "heowc", null, null)
+        def modifiedPost = new Post(1L, "수정된 제목", "수정된 본문", "heowc", null, null)
+
+        when:
+        post.changeFields(modifiedPost)
+
+        then:
+        with(post) {
+            id == modifiedPost.id
+            title == modifiedPost.title
+            content == modifiedPost.content
+            createdBy == modifiedPost.createdBy
+        }
+    }
+}
