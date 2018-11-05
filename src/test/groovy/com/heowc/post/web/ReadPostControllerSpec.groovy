a1f3c9b966906d3f55ebe581fd82b41b27a10320
heowc <heowc1992@gmail.com>
2018-11-05 11:27:32 +0900

    [ADD] posts/{id} 에 대한 테스트 케이스 목록 작성

---
+++ src/test/groovy/com/heowc/post/web/ReadPostControllerSpec.groovy
@@ -0,0 +1,21 @@
+package com.heowc.post.web
+
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class ReadPostControllerSpec extends Specification {
+
+    def "없는 id를 조회하여 실패"() {
+//        given:
+//        when:
+//        then:
+    }
+
+    def "해당 id에 대한 게시글이 존재하므로 Post 반환하며 성공"() {
+//        given:
+//        when:
+//        then:
+    }
+}
