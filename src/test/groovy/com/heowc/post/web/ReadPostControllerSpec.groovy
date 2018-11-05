1e7fc34fb7d32dbc3fa8eb2957975f315fc7e568
heowc <heowc1992@gmail.com>
2018-11-05 13:06:53 +0900

    [FIX] posts/{id} 에 대한 테스트 케이스 작성

--- src/test/groovy/com/heowc/post/web/ReadPostControllerSpec.groovy
+++ src/test/groovy/com/heowc/post/web/ReadPostControllerSpec.groovy
@@ -1,27 +1,69 @@
 package com.heowc.post.web
 
-
+import com.heowc.post.domain.Post
+import com.heowc.post.domain.PostRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.http.HttpStatus
+import org.springframework.web.util.UriComponentsBuilder
 import spock.lang.Specification
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ReadPostControllerSpec extends Specification {
 
-    def "없는 id를 조회하여 HttpStatus(400)를 반환하며 실패"() {
-//        given:
-//        when:
-//        then:
+    @Autowired
+    PostRepository repository
+
+    @LocalServerPort
+    def port
+
+    def restTemplate
+
+    def setup() {
+        def uriComponents = UriComponentsBuilder
+                .fromHttpUrl("http://localhost")
+                .port(port)
+                .build()
+
+        restTemplate = new RestTemplateBuilder().rootUri(uriComponents.toUriString()).build()
     }
 
-    def "id를 입력하지 않아 HttpStatus(404)를 반환하며 실패"() {
-//        given:
-//        when:
-//        then:
+    def "없는 id를 조회하여 HttpStatus(404)를 반환하며 실패"() {
+        when:
+        def entity = restTemplate.getForEntity("/posts/{id}", Post.class, 1L)
+
+        then:
+        entity.statusCode == HttpStatus.NOT_FOUND
     }
 
-    def "해당 id에 대한 게시글이 존재하므로 Post 반환하며 성공"() {
-//        given:
-//        when:
-//        then:
+    def "id를 입력하지 않아 HttpStatus(400)를 반환하며 실패"() {
+        given:
+        repository.save(new Post(1L, "제목", "본문", "heowc", null,
+                null))
+        when:
+        def entity = restTemplate.getForEntity("/posts/{id}", Post.class, 1L)
+
+        then:
+        entity.statusCode == HttpStatus.BAD_REQUEST
+    }
+
+    def "해당 id에 대한 게시글이 존재하므로 HttpStatus(200)과 Post 반환하며 성공"() {
+        given:
+        def post = new Post(1L, "제목", "본문", "heowc", null, null)
+        repository.save(post)
+
+        when:
+        def entity = restTemplate.getForEntity("/posts/{id}", Post.class, 1L)
+
+        then:
+        entity.statusCode == HttpStatus.OK
+        with(entity.getBody(), Post.class) {
+            post.id == id
+            post.title == title
+            post.content == content
+            post.createdBy == createdBy
+        }
     }
 }
