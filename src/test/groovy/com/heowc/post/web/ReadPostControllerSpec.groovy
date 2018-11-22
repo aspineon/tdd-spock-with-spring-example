f326d444126dd37a5ea587fa41b06b564e975eab
heowc <heowc1992@gmail.com>
2018-11-22 11:14:44 +0900

    [FIX] Session 설정 추가

--- src/test/groovy/com/heowc/post/web/ReadPostControllerSpec.groovy
+++ src/test/groovy/com/heowc/post/web/ReadPostControllerSpec.groovy
@@ -1,14 +1,18 @@
 package com.heowc.post.web
 
+import com.heowc.config.TestConfig
 import com.heowc.post.domain.Post
 import com.heowc.post.domain.PostRepository
+import com.heowc.util.SessionUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.context.annotation.Import
 import org.springframework.http.HttpStatus
 import spock.lang.Specification
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestConfig.class)
 class ReadPostControllerSpec extends Specification {
 
     @Autowired
@@ -18,6 +22,9 @@ class ReadPostControllerSpec extends Specification {
     TestRestTemplate restTemplate
 
     def "없는 id를 조회하여 HttpStatus(404)를 반환하며 실패"() {
+        given:
+        SessionUtils.setAttribute("ID", "heowc")
+
         when:
         def entity = restTemplate.getForEntity("/posts/{id}", Post.class, 1L)
 
@@ -27,6 +34,9 @@ class ReadPostControllerSpec extends Specification {
 
     def "올바르지 않은 id를 조회하여 HttpStatus(400)를 반환하며 실패"() {
         given:
+        SessionUtils.setAttribute("ID", "heowc")
+
+        and:
         repository.save(new Post(null, "제목", "본문", "heowc", null,
                 null))
         when:
@@ -38,6 +48,9 @@ class ReadPostControllerSpec extends Specification {
 
     def "해당 id에 대한 게시글이 존재하므로 HttpStatus(200)과 Post 반환하며 성공"() {
         given:
+        SessionUtils.setAttribute("ID", "heowc")
+
+        and:
         def post = repository.save(new Post(null, "제목", "본문", "heowc", null, null))
 
         when:
@@ -55,5 +68,6 @@ class ReadPostControllerSpec extends Specification {
 
     def cleanup() {
         repository.deleteAll()
+        SessionUtils.removeAttribute("ID")
     }
 }
